5fc34b53abe0a7437f9a5948fea1a53e8d888ee7
Evan Summers <evan.summers@gmail.com>
2017-01-21 18:48:06 +0000

    initial

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,9 +1,8 @@
 (
   set -u -e -x
-  network='test-hget-redis-network'
-  docker network create -d bridge test-hget-redis-network
-  container=`docker run --network=test-hget-redis-network \
-    --name test-redis-hget -d tutum/redis`
+  docker network create -d bridge 'test-hget-redis-network'
+  container=`docker run --network='test-hget-redis-network' \
+    --name 'test-redis-hget' -d tutum/redis`
   redisPass=`docker logs $container | grep '^\s*redis-cli -a' |
     sed -e 's/^\s*redis-cli -a \(\w*\) .*$/\1/'`
   redisHost=`docker inspect $container |
@@ -12,7 +11,7 @@
   redis-cli -a $redisPass -h $redisHost hset mytest:1001:h err some_error
   redis-cli -a $redisPass -h $redisHost hset mytest:1002:h err other_error
   redis-cli -a $redisPass -h $redisHost keys 'mytest:*:h'
-  docker run --network=test-hget-redis-network -e redisUrl=$redisUrl \
+  docker run --network='test-hget-redis-network' -e redisUrl=$redisUrl \
     -e pattern=mytest:*:h -e field=err evanxsummers/hget
   docker rm -f `docker ps -q -f name=test-redis-hget`
   docker network rm test-hget-redis-network
